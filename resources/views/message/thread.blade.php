ed530f7c5733abdf567973b558860b8eafdd9190
Aiwun <fristel@gmx.fr>
2021-02-11 18:49:23 +0000

    Modification du lien avatar (Gravatar)

--- resources/views/message/thread.blade.php
+++ resources/views/message/thread.blade.php
@@ -1,6 +1,6 @@
 <div class="mess__item">
     <div class="image img-cir img-40">
-        <img src="//www.gravatar.com/avatar/{{ md5($thread->creator()->email) }} ?s=64" alt="{{ $thread->creator()->name }}" />
+        <img src="//www.gravatar.com/avatar/{{ md5($thread->creator()->email) }}?s=64" alt="{{ $thread->creator()->name }}" />
     </div>
     <div class="content">
         <h6>{{ $thread->subject }}</h6>
